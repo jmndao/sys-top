4d9d6dc59763bce0b48608f156843dcf2a6496ed
jmndao <jmndao@gmail.com>
2021-08-13 16:21:19 +0000

    Saving Data to our Store/File

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -95,7 +95,34 @@ const secondsToDhms = (seconds) => {
     return `${d}d, ${h}h, ${m}m, ${s}s`;
 };
 
+// Get Settings
 ipcRenderer.on('settings:get', (e, settings) => {
     document.getElementById('cpu-overload').value = settings.cpuOverload;
     document.getElementById('alert-frequency').value = settings.alertFrequency;
 });
+
+// Alert function
+const showAlert = (msg) => {
+    const alert = document.getElementById('alert');
+    alert.classList.remove('hide');
+    alert.classList.add('alert');
+    alert.innerText = msg;
+
+    setTimeout(() => alert.classList.add('hide'), 3000);
+}
+
+// Submit Settings
+const settingsForm = document.getElementById('settings-form');
+settingsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const cpuOverload = document.getElementById('cpu-overload').value;
+    const alertFrequency = document.getElementById('alert-frequency').value;
+
+    // Send new settings to main Process
+    ipcRenderer.send('settings:set', {
+        cpuOverload,
+        alertFrequency
+    });
+
+    showAlert('Settings saved');
+})
